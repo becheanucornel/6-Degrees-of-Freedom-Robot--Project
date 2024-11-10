ec979823124f24060036f514bbcf77286f90853f
Becheanu Cornel-Constantin <128052570+becheanucornel@users.noreply.github.com>
2024-11-10 10:21:00 +0200

    Updated

--- animation.py
+++ animation.py
@@ -79,8 +79,8 @@ def main():
             return joint_positions, end_effector_pos
 
     # Define joint start and end angles
-    start_angle = np.array([0.9, -0.7, -2, 1, 0.8, 1])
-    finish_angle = np.array([3, -2, -0.8, -1, 1.2, 1])
+    start_angle = np.array([0.9, -0.4, -2, 1, 0.4, 1])
+    finish_angle = np.array([3, -2, -0.8, -1, 1.8, 1])
     animation_duration = 5  # Total animation time (in seconds)
 
     # Create time vector for the animation
